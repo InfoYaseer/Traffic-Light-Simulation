5bc1cb96751792139b3bc9dbb907cf6735c5b3af
InfoYaseer <160985940+InfoYaseer@users.noreply.github.com>
2024-11-02 16:24:19 +0530

    Add project files.

---
+++ Traffic ll Sm/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Traffic_ll_Sm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Yellowlight = new System.Windows.Forms.Panel();
+            this.GreenLight = new System.Windows.Forms.Panel();
+            this.RedLight = new System.Windows.Forms.Panel();
+            this.btnpress = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Yellowlight
+            // 
+            this.Yellowlight.Location = new System.Drawing.Point(283, 154);
+            this.Yellowlight.Name = "Yellowlight";
+            this.Yellowlight.Size = new System.Drawing.Size(92, 70);
+            this.Yellowlight.TabIndex = 0;
+            this.Yellowlight.Paint += new System.Windows.Forms.PaintEventHandler(this.Yellowlight_Paint);
+            // 
+            // GreenLight
+            // 
+            this.GreenLight.Location = new System.Drawing.Point(283, 268);
+            this.GreenLight.Name = "GreenLight";
+            this.GreenLight.Size = new System.Drawing.Size(92, 70);
+            this.GreenLight.TabIndex = 0;
+            this.GreenLight.Paint += new System.Windows.Forms.PaintEventHandler(this.GreenLight_Paint);
+            // 
+            // RedLight
+            // 
+            this.RedLight.Location = new System.Drawing.Point(283, 54);
+            this.RedLight.Name = "RedLight";
+            this.RedLight.Size = new System.Drawing.Size(92, 70);
+            this.RedLight.TabIndex = 0;
+            this.RedLight.Paint += new System.Windows.Forms.PaintEventHandler(this.RedLight_Paint);
+            // 
+            // btnpress
+            // 
+            this.btnpress.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpress.Location = new System.Drawing.Point(283, 350);
+            this.btnpress.Name = "btnpress";
+            this.btnpress.Size = new System.Drawing.Size(92, 61);
+            this.btnpress.TabIndex = 1;
+            this.btnpress.Text = "Press The Button";
+            this.btnpress.UseVisualStyleBackColor = true;
+            this.btnpress.Click += new System.EventHandler(this.btnpress_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 480);
+            this.Controls.Add(this.btnpress);
+            this.Controls.Add(this.RedLight);
+            this.Controls.Add(this.GreenLight);
+            this.Controls.Add(this.Yellowlight);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Yellowlight;
+        private System.Windows.Forms.Panel GreenLight;
+        private System.Windows.Forms.Panel RedLight;
+        private System.Windows.Forms.Button btnpress;
+    }
+}
+
---
+++ Traffic ll Sm/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traffic_ll_Sm
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int elapsedSecond = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTrafficLight();
+        }
+
+        private void InitializeTrafficLight()
+        {
+            RedLight.BackColor = Color.Red;
+            Yellowlight.BackColor = SystemColors.ControlDark;
+            GreenLight.BackColor = SystemColors.ControlDark;
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedSecond++;
+            if (elapsedSecond==1)
+            {
+                Yellowlight.BackColor = Color.Yellow;
+                RedLight.BackColor = SystemColors.ControlDark;
+
+            }
+            else if (elapsedSecond==5)
+            {
+                GreenLight.BackColor = Color.Green;
+                Yellowlight.BackColor = SystemColors.ControlDark;
+
+            }
+            else if (elapsedSecond == 15)
+            {
+                RedLight.BackColor = Color.Red;
+                GreenLight.BackColor = SystemColors.ControlDark;
+                elapsedSecond = 0;
+                timer.Stop();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Yellowlight_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void GreenLight_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void RedLight_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnpress_Click(object sender, EventArgs e)
+        {
+            elapsedSecond = 0;
+            timer.Start();
+
+        }
+    }
+}
